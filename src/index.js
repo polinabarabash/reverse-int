49049ce92e673157e48822c331a4d3267b629cea
Polina Barabash <polina_11.92@inbox.ru>
2020-03-17 21:41:24 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split(''); 
+    if (arr[0] === '-') {
+        arr.shift();
+    }   
+    let rez = arr.reverse().join('');          
+    return Number(rez);
 }
